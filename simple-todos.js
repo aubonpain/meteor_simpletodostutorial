0d68e42ddb231eced5c5a7ce418ea2d339cc1ec3
aubonpain <abi.sampath@gmail.com>
2015-10-25 16:54:31 -0400

    Worked through adding list w/ checked and delete

--- simple-todos.js
+++ simple-todos.js
@@ -1,21 +1,61 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
+
     Template.body.helpers({
-        tasks: [
-            { text: "This is task 1" },
+        tasks: function () {
+            // What about returning multiple things? E.g. date of creation etc.
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+
+        /*tasks: [
+            { text: "This is first"  },
             { text: "This is task 2" },
             { text: "This is task 3" }
         ],
 
         places: [
             { text: "Bombay" },
-            { text: "" },
-            { text: "This is task 3" }
-        ],
+            { text: "Zurich" },
+            { text: "Tunis" }
+        ]*/
 
-        images: [
+/*        images: [
             { image   : "/images/mercury/mercury.jpg" },
             { image   : "/images/venus/venus.jpg" }
-        ]
+        ]*/
+    });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        },
+
+        "click .delete": function () {
+            // _id = "ObjectId" of that record (unique)
+            Tasks.remove(this._id);
+        },
+
+        "click .toggle-selected": function () {
+            Tasks.update(this._id, {
+                // $set changes the value of the specified field
+                // If that field doesn't exist, it'll create it.
+                $set: {checked: ! this.checked}
+            });
+        }
     });
 }
 
